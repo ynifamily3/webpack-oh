17863bab58ec48e9a2d9df8141d0fe116cd5f8bb
Nexon <nexon@NX-282049.local>
2023-10-17 19:24:30 +0900

    add webpack & ts settings

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import type { APIGatewayProxyHandlerV2, APIGatewayProxyStructuredResultV2 } from "aws-lambda";
+import { obj } from "./projects";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     const { headers } = event;
 
     const host = headers['x-forwarded-host'];
+    const haha = obj.haha;
 
     const response: APIGatewayProxyStructuredResultV2 = {
         statusCode: 200,
         headers: {
             'content-type': 'text/html; charset=UTF-8'
         },
-        body: JSON.stringify({ message: 'Hello, Babo World!', host})
+        body: JSON.stringify({ message: 'Hello, Babo World!', host, haha})
     };
     return response;
 };
